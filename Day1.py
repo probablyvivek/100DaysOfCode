27d0c10eff64567171d6165eb601c705e43e2fed
Vivek Tiwari <viv.tiwari13@gmail.com>
2021-03-04 23:01:15 +0530

    Create Day1.py

---
+++ Day1.py
@@ -0,0 +1,11 @@
+#print on different lines
+print('Day 1 - Python Print Function\n The Function is declared like this:\n  print what to print')
+
+#Input Function 
+name = input("What is your name? ")
+print("Hello " + name + "!")
+
+#Return Lenght of the Name 
+name = input("What is your name? ")
+print("Hello " + name + "!")
+print("The lenght of your name is " + str(len(name)))
